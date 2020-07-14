ca5333bbc7fbb0511d8393b40a2d4b0d905b32b2
Amanda Walker <walker.akw@gmail.com>
2020-07-14 18:48:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
     @this_dogs_name = dogs_name
   end #setter/writer
 
-  def name(dogs_name)
-    @this_dogs_name
-  end #getter/reader
+  # def name(dogs_name)
+  #   @this_dogs_name
+  # end #getter/reader
 end
